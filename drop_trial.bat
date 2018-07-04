be52f27524e1cdd61d6536e8b4a2542856113c1f
Oleksandr Dmytrenko <odmitrenko1@gmail.com>
2018-07-04 18:22:49 +0300

    Added logic to take trial reset key

--- drop_trial.bat
+++ drop_trial.bat
@@ -1,12 +1,14 @@
 @echo off
 
-taskkill /t /f /im "pdapp.exe"
-taskkill /t /f /im "Adobe*"
-taskkill /t /f /im "CCLibrary.exe"
-taskkill /t /f /im "CCXProcess.exe"
-taskkill /t /f /im "CoreSync.exe"
-taskkill /t /f /im "Creative Cloud.exe"
+REM taskkill /t /f /im "pdapp.exe"
+REM taskkill /t /f /im "Adobe*"
+REM taskkill /t /f /im "CCLibrary.exe"
+REM taskkill /t /f /im "CCXProcess.exe"
+REM taskkill /t /f /im "CoreSync.exe"
+REM taskkill /t /f /im "Creative Cloud.exe"
 
 set DATA_DIR="C:\Program Files\Adobe\Adobe Photoshop CC*\AMT"
+cd %DATA_DIR%
+sed -i -E "s/(.*)(<Data.*key=\"TrialSerialNumber\">)([0-9]+)(.*)/\1\2$((\3+1))\4/e" application.xml
 
-dir %DATA_DIR%\AMT
+yolo=`awk "/TrialSerialNumber/{print $1;}" application.xml | awk '{split($0,parts,"(<Data key=\"TrialSerialNumber\">)|(</Data></Other>)"); { print parts[2]}}'`
